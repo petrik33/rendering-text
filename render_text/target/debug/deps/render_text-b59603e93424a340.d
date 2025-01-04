d2eabc5882f72f12af2e0c122c72f920fe419158
Tsimafei Petrykevich <petrik3003@gmail.com>
2025-01-05 01:06:06 +0300

    Upload full workspace

---
+++ render_text/target/debug/deps/render_text-b59603e93424a340.d
@@ -0,0 +1,5 @@
+/home/petrik33/Pet/Coding Adventures/Text Rendering/rendering-text/render_text/target/debug/deps/librender_text-b59603e93424a340.rmeta: src/lib.rs
+
+/home/petrik33/Pet/Coding Adventures/Text Rendering/rendering-text/render_text/target/debug/deps/render_text-b59603e93424a340.d: src/lib.rs
+
+src/lib.rs:
---
+++ render_text/target/debug/deps/render_text-c58dea811bcfa1c5.d
@@ -0,0 +1,5 @@
+/home/petrik33/Pet/Coding Adventures/Text Rendering/rendering-text/render_text/target/debug/deps/librender_text-c58dea811bcfa1c5.rmeta: src/lib.rs
+
+/home/petrik33/Pet/Coding Adventures/Text Rendering/rendering-text/render_text/target/debug/deps/render_text-c58dea811bcfa1c5.d: src/lib.rs
+
+src/lib.rs:
